2324442ef88b31d5c17fc97634dff695bd770f79
Scott S <shadownix@gmail.com>
2013-01-30 13:56:19 -0800

    Added Menu, Pause

--- spiral_1/hello_world.c
+++ spiral_1/hello_world.c
@@ -11,10 +11,12 @@
 #define YOUR_SRAM_ADDR 			0x80000
 
 //Change this to your local SW address
-#define YOUR_SWITCHES_ADDR		0x4010//0x0002000
+//#define YOUR_SWITCHES_ADDR		0x2000
+#define YOUR_SWITCHES_ADDR		0x4010
 
 //Change to your QSYS local name
-#define YOUR_PIXEL_BUFFER_NAME 	"/dev/pixel_buffer_dma"//"/dev/video_pixel_buffer_dma_0"
+//#define YOUR_PIXEL_BUFFER_NAME 	"/dev/video_pixel_buffer_dma_0"
+#define YOUR_PIXEL_BUFFER_NAME 	"/dev/pixel_buffer_dma"
 
 //Change to your QSYS local name
 #define YOUR_CHAR_BUFFER_NAME 	"/dev/char_drawer"
@@ -38,6 +40,8 @@ typedef enum { NoBlock, Grass, Water, Highway, WinBlock } background;
 
 #define gridx 16
 #define gridy 10
+
+int pause();
 //------------------------------------------------------//
 
 
@@ -45,7 +49,7 @@ typedef enum { NoBlock, Grass, Water, Highway, WinBlock } background;
 
 
 //------------------------------------------------------\\
-//Global Variables
+// Global Variables
 
 	alt_up_pixel_buffer_dma_dev* pixel_buffer;
 	alt_up_char_buffer_dev *char_buffer;
@@ -220,7 +224,7 @@ dir getdir(){
 	}
 }
 
-void draw_frogger(){
+int draw_frogger(){
 	dir mydir = getdir();
 	if(mydir == up){
 		if(frog_y > 24){
@@ -234,21 +238,22 @@ void draw_frogger(){
 	}else if(mydir == left){
 		if(frog_x > 0){
 			frog_x = frog_x-20;}
+	}else if (mydir == dpause){
+		switch (pause()){
+			case 0: return 0;
+			case 1: return 1;
+			case 3: return 0;
+		}
 	}
 	alt_up_pixel_buffer_dma_draw_box(pixel_buffer, frog_x, frog_y, frog_x+20, frog_y+24, 0xF000, 1);
+	return 0;
 }
 
 //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-
-int main(){
-
-	initilize_vga();
+int mainrun(){
 
 //Initilize Variables
-	int x;
 	int time_var2 =0;
-	int sw0;
-	int i,j =0;
 	int truck_1 = 0;
 	int truck_2 = 160;
 
@@ -267,7 +272,7 @@ int main(){
 		truck_1 = draw_truck(truck_1, 192, 1);
 		truck_2 = draw_truck(truck_2, 192, 1);
 
-		draw_frogger();
+		if ( draw_frogger() ) break;
 
 		alt_up_pixel_buffer_dma_swap_buffers(pixel_buffer);
 
@@ -289,6 +294,200 @@ int main(){
 			}
 		}
 
+	}
+	return 0;
+}
+
+int highscores(){
+	int i;
+	alt_up_pixel_buffer_dma_draw_box(pixel_buffer, 0, 0, 320, 240, 0x0000, 0);
+	alt_up_char_buffer_string(char_buffer, "High Scores unavailable.", 30, 30);
+	for (i=0; i<10000000; i++);
+return 0;}
+
+int options(){
+	int i;
+	alt_up_pixel_buffer_dma_draw_box(pixel_buffer, 0, 0, 320, 240, 0x0000, 0);
+	alt_up_char_buffer_string(char_buffer, "Options unavailable.", 30, 30);
+	for (i=0; i<10000000; i++);
+return 0;}
+
+int win(){
+	int i;
+	alt_up_pixel_buffer_dma_draw_box(pixel_buffer, 0, 0, 320, 240, 0x0000, 0);
+	alt_up_char_buffer_string(char_buffer, "YOU WIN!", 30, 30);
+	for (i=0; i<10000000; i++);
+return 0;}
+
+int die(){
+	int i;
+	alt_up_pixel_buffer_dma_draw_box(pixel_buffer, 0, 0, 320, 240, 0x0000, 0);
+	alt_up_char_buffer_string(char_buffer, "YOU LOSE!", 30, 30);
+	for (i=0; i<10000000; i++);
+return 0;}
+
+int menu(){
+	unsigned int sel = 999;
+	int x = 80;
+
+	alt_up_char_buffer_clear(char_buffer);
+	alt_up_pixel_buffer_dma_draw_box(pixel_buffer, 0, 0, 320, 240, 0x0000, 0);
+	alt_up_char_buffer_string(char_buffer, "---MENU---", 30, 10);
+	alt_up_char_buffer_string(char_buffer, "Play", 30, 26);
+	alt_up_char_buffer_string(char_buffer, "High Scores", 30, 39);
+	alt_up_char_buffer_string(char_buffer, "Options", 30, 51);
+
+	if (sel%3 == 0){
+		alt_up_pixel_buffer_dma_draw_box(pixel_buffer, x, 100, x+10, 110, 0xf000, 0);
+		alt_up_pixel_buffer_dma_draw_box(pixel_buffer, x, 150, x+10, 160, 0x0000, 0);
+		alt_up_pixel_buffer_dma_draw_box(pixel_buffer, x, 200, x+10, 210, 0x0000, 0);
+	}else if (sel%3 == 1){
+		alt_up_pixel_buffer_dma_draw_box(pixel_buffer, x, 100, x+10, 110, 0x0000, 0);
+		alt_up_pixel_buffer_dma_draw_box(pixel_buffer, x, 150, x+10, 160, 0xf000, 0);
+		alt_up_pixel_buffer_dma_draw_box(pixel_buffer, x, 200, x+10, 210, 0x0000, 0);
+	}else if (sel%3 == 2){
+		alt_up_pixel_buffer_dma_draw_box(pixel_buffer, x, 100, x+10, 110, 0x0000, 0);
+		alt_up_pixel_buffer_dma_draw_box(pixel_buffer, x, 150, x+10, 160, 0x0000, 0);
+		alt_up_pixel_buffer_dma_draw_box(pixel_buffer, x, 200, x+10, 210, 0xf000, 0);
+	}
+
+	for(;;){
+		dir mydir = getdir();
+		if (mydir == down) {
+			sel++;
+			if (sel%3 == 0){
+					alt_up_pixel_buffer_dma_draw_box(pixel_buffer, x, 100, x+10, 110, 0xf000, 0);
+					alt_up_pixel_buffer_dma_draw_box(pixel_buffer, x, 150, x+10, 160, 0x0000, 0);
+					alt_up_pixel_buffer_dma_draw_box(pixel_buffer, x, 200, x+10, 210, 0x0000, 0);
+				}else if (sel%3 == 1){
+					alt_up_pixel_buffer_dma_draw_box(pixel_buffer, x, 100, x+10, 110, 0x0000, 0);
+					alt_up_pixel_buffer_dma_draw_box(pixel_buffer, x, 150, x+10, 160, 0xf000, 0);
+					alt_up_pixel_buffer_dma_draw_box(pixel_buffer, x, 200, x+10, 210, 0x0000, 0);
+				}else if (sel%3 == 2){
+					alt_up_pixel_buffer_dma_draw_box(pixel_buffer, x, 100, x+10, 110, 0x0000, 0);
+					alt_up_pixel_buffer_dma_draw_box(pixel_buffer, x, 150, x+10, 160, 0x0000, 0);
+					alt_up_pixel_buffer_dma_draw_box(pixel_buffer, x, 200, x+10, 210, 0xf000, 0);
+				}
+
+		}
+		else if (mydir == up) {
+			sel--;
+			if (sel%3 == 0){
+					alt_up_pixel_buffer_dma_draw_box(pixel_buffer, x, 100, x+10, 110, 0xf000, 0);
+					alt_up_pixel_buffer_dma_draw_box(pixel_buffer, x, 150, x+10, 160, 0x0000, 0);
+					alt_up_pixel_buffer_dma_draw_box(pixel_buffer, x, 200, x+10, 210, 0x0000, 0);
+				}else if (sel%3 == 1){
+					alt_up_pixel_buffer_dma_draw_box(pixel_buffer, x, 100, x+10, 110, 0x0000, 0);
+					alt_up_pixel_buffer_dma_draw_box(pixel_buffer, x, 150, x+10, 160, 0xf000, 0);
+					alt_up_pixel_buffer_dma_draw_box(pixel_buffer, x, 200, x+10, 210, 0x0000, 0);
+				}else if (sel%3 == 2){
+					alt_up_pixel_buffer_dma_draw_box(pixel_buffer, x, 100, x+10, 110, 0x0000, 0);
+					alt_up_pixel_buffer_dma_draw_box(pixel_buffer, x, 150, x+10, 160, 0x0000, 0);
+					alt_up_pixel_buffer_dma_draw_box(pixel_buffer, x, 200, x+10, 210, 0xf000, 0);
+				}
+
+		}
+		else if (mydir == left || mydir == right){
+			if (sel%3 == 0){
+				alt_up_char_buffer_clear(char_buffer);
+				mainrun();
+			}else if (sel%3 == 1){
+				alt_up_char_buffer_clear(char_buffer);
+				highscores();
+			}else if (sel%3 == 2){
+				alt_up_char_buffer_clear(char_buffer);
+				options();
+			}
+			break;
+		}
+	}
+return 0;}
+
+
+int pause(){
+	int sel = 999;
+	int x = 180;
+	alt_up_pixel_buffer_dma_draw_box(pixel_buffer, (320/gridx)*(gridx/3), 0, (320/gridx)*(2*gridx/3), 240, 0x0000, 0);
+	alt_up_char_buffer_string(char_buffer, "---PAUSE---", 30, 10);
+	alt_up_char_buffer_string(char_buffer, "Resume Game", 30, 26);
+	alt_up_char_buffer_string(char_buffer, "Quit Game", 30, 39);
+	alt_up_char_buffer_string(char_buffer, "Options", 30, 51);
+
+	if (sel%3 == 0){
+			alt_up_pixel_buffer_dma_draw_box(pixel_buffer, x, 100, x+10, 110, 0xf000, 0);
+			alt_up_pixel_buffer_dma_draw_box(pixel_buffer, x, 150, x+10, 160, 0x0000, 0);
+			alt_up_pixel_buffer_dma_draw_box(pixel_buffer, x, 200, x+10, 210, 0x0000, 0);
+		}else if (sel%3 == 1){
+			alt_up_pixel_buffer_dma_draw_box(pixel_buffer, x, 100, x+10, 110, 0x0000, 0);
+			alt_up_pixel_buffer_dma_draw_box(pixel_buffer, x, 150, x+10, 160, 0xf000, 0);
+			alt_up_pixel_buffer_dma_draw_box(pixel_buffer, x, 200, x+10, 210, 0x0000, 0);
+		}else if (sel%3 == 2){
+			alt_up_pixel_buffer_dma_draw_box(pixel_buffer, x, 100, x+10, 110, 0x0000, 0);
+			alt_up_pixel_buffer_dma_draw_box(pixel_buffer, x, 150, x+10, 160, 0x0000, 0);
+			alt_up_pixel_buffer_dma_draw_box(pixel_buffer, x, 200, x+10, 210, 0xf000, 0);
+		}
+
+		for(;;){
+			dir mydir = getdir();
+			if (mydir == down) {
+				sel++;
+				if (sel%3 == 0){
+						alt_up_pixel_buffer_dma_draw_box(pixel_buffer, x, 100, x+10, 110, 0xf000, 0);
+						alt_up_pixel_buffer_dma_draw_box(pixel_buffer, x, 150, x+10, 160, 0x0000, 0);
+						alt_up_pixel_buffer_dma_draw_box(pixel_buffer, x, 200, x+10, 210, 0x0000, 0);
+					}else if (sel%3 == 1){
+						alt_up_pixel_buffer_dma_draw_box(pixel_buffer, x, 100, x+10, 110, 0x0000, 0);
+						alt_up_pixel_buffer_dma_draw_box(pixel_buffer, x, 150, x+10, 160, 0xf000, 0);
+						alt_up_pixel_buffer_dma_draw_box(pixel_buffer, x, 200, x+10, 210, 0x0000, 0);
+					}else if (sel%3 == 2){
+						alt_up_pixel_buffer_dma_draw_box(pixel_buffer, x, 100, x+10, 110, 0x0000, 0);
+						alt_up_pixel_buffer_dma_draw_box(pixel_buffer, x, 150, x+10, 160, 0x0000, 0);
+						alt_up_pixel_buffer_dma_draw_box(pixel_buffer, x, 200, x+10, 210, 0xf000, 0);
+					}
+
+			}
+			else if (mydir == up) {
+				sel--;
+				if (sel%3 == 0){
+						alt_up_pixel_buffer_dma_draw_box(pixel_buffer, x, 100, x+10, 110, 0xf000, 0);
+						alt_up_pixel_buffer_dma_draw_box(pixel_buffer, x, 150, x+10, 160, 0x0000, 0);
+						alt_up_pixel_buffer_dma_draw_box(pixel_buffer, x, 200, x+10, 210, 0x0000, 0);
+					}else if (sel%3 == 1){
+						alt_up_pixel_buffer_dma_draw_box(pixel_buffer, x, 100, x+10, 110, 0x0000, 0);
+						alt_up_pixel_buffer_dma_draw_box(pixel_buffer, x, 150, x+10, 160, 0xf000, 0);
+						alt_up_pixel_buffer_dma_draw_box(pixel_buffer, x, 200, x+10, 210, 0x0000, 0);
+					}else if (sel%3 == 2){
+						alt_up_pixel_buffer_dma_draw_box(pixel_buffer, x, 100, x+10, 110, 0x0000, 0);
+						alt_up_pixel_buffer_dma_draw_box(pixel_buffer, x, 150, x+10, 160, 0x0000, 0);
+						alt_up_pixel_buffer_dma_draw_box(pixel_buffer, x, 200, x+10, 210, 0xf000, 0);
+					}
+
+			}
+			else if (mydir == left || mydir == right){
+				if (sel%3 == 0){
+					alt_up_char_buffer_clear(char_buffer);
+					return 0;
+				}else if (sel%3 == 1){
+					//alt_up_char_buffer_clear(char_buffer);
+					return 1;
+				}else if (sel%3 == 2){
+					alt_up_char_buffer_clear(char_buffer);
+					return 2;
+				}
+				break;
+			}
+		}
+
+return 0;}
+
+
+int main(){
+	initilize_vga();
+	for(;;) {
+		menu();
+
+		// Clean up: put global variables back to starting values
+		// or else we restart the game dead
 	}
 	return 0;
 }
