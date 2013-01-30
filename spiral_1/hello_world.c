5deda1d92f158e7473aa1b79a90dcd2ec467f726
Scott S <shadownix@gmail.com>
2013-01-30 11:45:01 -0800

    New file for functions

--- spiral_1/hello_world.c
+++ spiral_1/hello_world.c
@@ -3,11 +3,11 @@
 #include <altera_up_avalon_video_pixel_buffer_dma.h>
 #include <altera_up_avalon_video_character_buffer_with_dma.h>
 
-//------------------------------------------------------
+//------------------------------------------------------\\
 //F_LOCAL:
 
-//Change this in your local SD ram address in QSYS
-#define YOUR_SDRAM_ADDR 		0x80000
+//Change this in your local S ram address in QSYS
+#define YOUR_SRAM_ADDR 		0x80000
 
 //Change this to your local SW address
 #define YOUR_SWITCHES_ADDR		0x0004010
@@ -18,18 +18,25 @@
 //Change to your QSYS local name
 #define YOUR_CHAR_BUFFER_NAME 	"/dev/char_drawer"
 
+//------------------------------------------------------//
 
-//------------------------------------------------------
 
-//Initilize Pixel and Character Buffer
-alt_up_pixel_buffer_dma_dev* pixel_buffer;
-alt_up_char_buffer_dev *char_buffer;
 
-//Global matrix
-int movement_matrix[10][16];
 
-//Global Time
-int time_var1 = 150;
+
+//------------------------------------------------------\\
+//Global Variables
+
+	alt_up_pixel_buffer_dma_dev* pixel_buffer;
+	alt_up_char_buffer_dev *char_buffer;
+
+	int movement_matrix[10][16];
+	int time_var1 = 150;
+	int truck_1=0;
+	int truck_2=160;
+
+//------------------------------------------------------//
+
 
 void init_matrix(){
     int i, j = 0;
@@ -45,8 +52,8 @@ void initilize_vga(){
 	// Use the name of your pixel buffer DMA core
 	pixel_buffer = alt_up_pixel_buffer_dma_open_dev(YOUR_PIXEL_BUFFER_NAME);
 
-	unsigned int pixel_buffer_addr1 = YOUR_SDRAM_ADDR;
-	unsigned int pixel_buffer_addr2 = YOUR_SDRAM_ADDR + (320 * 240 * 2);
+	unsigned int pixel_buffer_addr1 = YOUR_SRAM_ADDR;
+	unsigned int pixel_buffer_addr2 = YOUR_SRAM_ADDR + (320 * 240 * 2);
 
 	// Set the 1st buffer address
 	alt_up_pixel_buffer_dma_change_back_buffer_address(pixel_buffer, pixel_buffer_addr1);
@@ -64,7 +71,7 @@ void initilize_vga(){
 	alt_up_pixel_buffer_dma_clear_screen(pixel_buffer, 1);
 
 	// Initialize
-	char_buffer = alt_up_char_buffer_open_dev("/dev/char_drawer");
+	char_buffer = alt_up_char_buffer_open_dev(YOUR_CHAR_BUFFER_NAME);
 	alt_up_char_buffer_init(char_buffer);
 
 	// Clear the screen
@@ -98,8 +105,7 @@ int main(){
 
 //Initilize Variables
 
-	int truck_1=0;
-	int truck_2=160;
+
 	int x;
 	int time_var2 =0;
 	int frog_x = 120;
@@ -114,58 +120,7 @@ int main(){
 
         init_matrix();
         draw_background();
-
-
-//START - Trucks
-	//Truck 1
-		if(truck_1 >= 20){
-			alt_up_pixel_buffer_dma_draw_box(pixel_buffer, truck_1-20, 192, truck_1, 215, 0xFF00, 1);
-		}else{
-			alt_up_pixel_buffer_dma_draw_box(pixel_buffer, 0, 192, truck_1, 215, 0xFF00, 1);
-		}
-		if(truck_1 >= 40){
-			alt_up_pixel_buffer_dma_draw_box(pixel_buffer, truck_1-40, 192, truck_1-20, 215, 0xFFFF, 1);
-		}else if(truck_1 > 20 && truck_1 < 40){
-			alt_up_pixel_buffer_dma_draw_box(pixel_buffer, 0, 192, truck_1-20, 215, 0xFFFF, 1);
-		}
-		if (truck_1 >= 60){
-			alt_up_pixel_buffer_dma_draw_box(pixel_buffer, truck_1-60, 192, truck_1-40, 215, 0xFFFF, 1);
-		}else if(truck_1 > 40 && truck_1 < 60){
-			alt_up_pixel_buffer_dma_draw_box(pixel_buffer, 0, 192, truck_1-40, 215, 0xFFFF, 1);
-		}
-
-		truck_1=truck_1+1;
-		if(truck_1 == 380){truck_1=0;}
-		movement_matrix[192/24][(truck_1)/20] = 1;
-		if (truck_1 >= 20){movement_matrix[192/24][(truck_1-20)/20] = 1;}
-		if (truck_1 >= 40){movement_matrix[192/24][(truck_1-40)/20] = 1;}
-	//Truck 2
-		if(truck_2 >= 20){
-			alt_up_pixel_buffer_dma_draw_box(pixel_buffer, truck_2-20, 192, truck_2, 215, 0xFF00, 1);
-		}else{
-			alt_up_pixel_buffer_dma_draw_box(pixel_buffer, 0, 192, truck_2, 215, 0xFF00, 1);
-		}
-		if(truck_2 >= 40){
-			alt_up_pixel_buffer_dma_draw_box(pixel_buffer, truck_2-40, 192, truck_2-20, 215, 0xFFFF, 1);
-		}else if(truck_2 > 20 && truck_2 < 40){
-			alt_up_pixel_buffer_dma_draw_box(pixel_buffer, 0, 192, truck_2-20, 215, 0xFFFF, 1);
-		}
-		if (truck_2 >= 60){
-			alt_up_pixel_buffer_dma_draw_box(pixel_buffer, truck_2-60, 192, truck_2-40, 215, 0xFFFF, 1);
-		}else if(truck_2 > 40 && truck_2 < 60){
-			alt_up_pixel_buffer_dma_draw_box(pixel_buffer, 0, 192, truck_2-40, 215, 0xFFFF, 1);
-		}
-
-		truck_2=truck_2+1;
-		if(truck_2 == 380){
-			truck_2=0;
-		}
-
-		movement_matrix[192/24][(truck_2)/20] = 1;
-		if (truck_2 >= 20){movement_matrix[192/24][(truck_2-20)/20] = 1;}
-		if (truck_2 >= 40){movement_matrix[192/24][(truck_2-40)/20] = 1;}
-
-//FINISH Trucks
+		trucks_test();
 
 
 //START - Frogger
