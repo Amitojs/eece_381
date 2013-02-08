a8283416cfa71e47b5cc2a4236430989cb754d27
Scott S <shadownix@gmail.com>
2013-02-08 10:52:59 -0800

    save.h

--- spiral_1/hello_world.c
+++ spiral_1/hello_world.c
@@ -11,29 +11,13 @@
 #include "main.h"
 #include "draw_level.h"
 #include "menus.h"
-
+#include "save.h"
 
 
 
 //------------------------------------------------------
 // Global Variables
-
-    int issavegame = 0;
     dir lastdir;
-
-//-------------------------
-//    Save variables
-
-    int s_time_var1;
-    int s_frog_x;
-    int s_frog_y;
-    int s_lives_remaining;
-    int s_time_var2;
-    int s_truck_1,	s_truck_2;
-
-    int s_cars[numcars];
-	int s_logs[numlogs];
-
 //------------------------------------------------------
 
 void init_matrix(){
@@ -42,56 +26,6 @@ void init_matrix(){
         for (j=0; j<gridx; j++){
             movement_matrix[i][j] = 0;
         }
-    }
-}
-
-void savegame(){
-	int i;
-    issavegame = 1;
-    //Save the status of the game to be loaded
-    s_time_var1 = time_var1;
-    s_frog_x = frog_x;
-    s_frog_y = frog_y;
-    s_lives_remaining = lives_remaining;
-    s_time_var2 = time_var2;
-
-    s_truck_1 = truck_1;
-    s_truck_2 = truck_2;
-
-    for (i = 0; i <= numcars; i++){
-    	s_cars[i] = cars[i];
-    }
-	for (i = 0; i <= numlogs; i++){
-    	s_logs[i] = logs[i];
-    }
-}
-
-void loadgame(){
-    int i;
-
-    //Check save game
-    if (issavegame == 0){
-        alt_up_pixel_buffer_dma_draw_box(pixel_buffer, 0, 0, 320, 240, 0x0000, 0);
-        alt_up_char_buffer_string(char_buffer, "No game available.", 30, 30);
-        for (i=0; i<10000000; i++);
-    }else{
-        time_var1 = s_time_var1;
-        frog_x = s_frog_x;
-        frog_y = s_frog_y;
-        lives_remaining = s_lives_remaining;
-        time_var2 = s_time_var2;
-        truck_1 = s_truck_1;
-        truck_2 = s_truck_2;
-
-        for (i = 0; i <= numcars; i++){
-            cars[i] = s_cars[i];
-        }
-
-        for (i = 0; i <= numlogs; i++){
-            logs[i] = s_logs[i];
-        }
-
-        playgame();
     }
 }
 
