a6a14d018f96186bec2d63c98ae4ba9cb0ee1004
Scott S <shadownix@gmail.com>
2013-02-13 19:10:06 -0800

    lolwhat

--- spiral_1/main.h
+++ spiral_1/main.h
@@ -1,13 +1,16 @@
 #ifndef MAIN_H_
 #define MAIN_H_
 
+#include "altera_up_avalon_ps2.h"
+#include "altera_up_ps2_keyboard.h"
+
 //------------------------------------------------------
 //Directions
-typedef enum { up, down, left, right, nodir, dpause } dir;
+typedef enum { up, down, left, right, nodir, dpause, changedir, dontchangedir } dir;
 //------------------------------------------------------
 typedef enum { NoBlock, Grass, Water, Highway, WinBlock, Highway_t } background;
 background g[50][50];
-
+int keyb_set;
 char score[20];
 char temp_highscore[20];
 
@@ -32,5 +35,8 @@ int win_menu();
 void init_variables();
 void frog_bmp_init();
 void frog_bmp_draw();
+
+dir getDirectionFromKeyboard(dir lastdir, dir secondlastdir);
+void initialize_ps2 ();
 
 #endif /* MAIN_H_ */
